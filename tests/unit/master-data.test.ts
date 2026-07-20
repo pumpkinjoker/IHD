7c6c3953fff713b3105a4578973d47674fb75364
PumpkinJoker <thanakorn.foo@gmail.com>
2026-07-20 13:33:36 +0700

    chore: update expense request team master data

--- tests/unit/master-data.test.ts
+++ tests/unit/master-data.test.ts
@@ -3,15 +3,37 @@ import { findRequesterByKey, masterData } from "@/lib/master-data";
 
 describe("master data", () => {
   it("loads validated team and approver data", () => {
-    expect(masterData.team).toHaveLength(1);
+    expect(masterData.team).toHaveLength(5);
     expect(masterData.approvers.authorizedBy.position).toBe(
       "Head of marketing"
     );
   });
 
+  it("sorts team members by Thai first name order", () => {
+    expect(masterData.team.map((member) => member.firstName)).toEqual([
+      "ชญานิน",
+      "ชรีพร",
+      "ชินวัตร",
+      "ธนากร",
+      "บรรณพรต"
+    ]);
+  });
+
   it("finds requester by stable employee id key", () => {
+    expect(findRequesterByKey("11200462")?.email).toBe(
+      "chayanin_J@toagroup.com"
+    );
+    expect(findRequesterByKey("11240219")?.email).toBe(
+      "phatchareepron_l@toagroup.com"
+    );
+    expect(findRequesterByKey("11240752")?.email).toBe(
+      "bunaprot_b@toagroup.com"
+    );
     expect(findRequesterByKey("60112369")?.email).toBe(
       "thanakorn_f@toagroup.com"
+    );
+    expect(findRequesterByKey("11240248")?.email).toBe(
+      "chinawat_t@toagroup.com"
     );
   });
 });
